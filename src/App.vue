2064f3c09ec408c79502a560e28ff29d3206b294
olyefimchuk <oyefimchuk@educa.madrid.org>
2025-08-11 13:58:41 +0200

    commit de proyecto

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+    <h1>CRYPTO</h1>
+    <Input :changeAmount="changeAmount" />
+    <p v-if="error != ''" class="error">{{ error }}</p>
+    
+    
+    <div class="selectors">
+        <Selector :setCrypto="setCryptoFirst" />
+        <Selector :setCrypto="setCryptoSecond" />
+    </div>
+    <input type="number" class="inputResult" v-model="info" placeholder="Resultado de la conversión" readonly />
+    <button class="buttonConvert" @click="convert()">Convertir</button>
+
+</template>
+
+
+
+<script>
+import Input from "./components/Input.vue"
+import Selector from "./components/Selector.vue"
+import axios from "axios"
+
+export default {
+    components: { Input, Selector },
+    data() {
+        return {
+            amount: 0,
+            cryptoFirst: "",
+            cryptoSecond: "",
+            error: "",
+            info: null
+        }
+    },
+    methods: {
+        changeAmount(val) {
+            this.amount = val
+        },
+        setCryptoFirst(val) {
+            this.cryptoFirst = val
+        },
+        setCryptoSecond(val) {
+            this.cryptoSecond = val
+        },
+
+        convert() {
+            this.errorMessage()
+            if(this.error) {
+                return; // Finalizar ejecucion si hay un error
+            }
+
+            const apiUrl = `https://api.coinconvert.net/convert/${this.cryptoFirst}/${this.cryptoSecond}?amount=${this.amount}`
+            axios.get(apiUrl)
+                .then(response => {this.info = response.data[this.cryptoSecond]})
+                .catch(error => {
+                    console.error("Error al realizar la conversión:", error);
+                    this.error = "Error al realizar la conversión";
+                });
+        },
+
+        errorMessage() {
+            if(this.amount == "") {
+                this.error = "Introduce un numero"
+            }
+            else if(this.amount <= 0) {
+                this.error = "Numero no puede ser menor o igual a 0"
+            }
+            else if(this.cryptoFirst == "" || this.cryptoSecond == "") {
+                this.error = "Selecciona las criptomonedas"
+            }
+            else if(this.cryptoFirst == this.cryptoSecond) {
+                this.error = "Las criptomonedas no pueden ser iguales"
+            }
+            else {
+                this.error = ""
+            }
+        }
+    }
+}
+
+
+
+</script>
+
+
+
+<style scoped>
+.selectors {
+    display: flex;
+    justify-content: space-around;
+    width: 700px;
+    margin: 0 auto;
+}
+
+.buttonConvert {
+    display: block;
+    width: 200px;
+    margin: 20px auto;
+    padding: 10px;
+    background: #1a032d;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.buttonConvert:hover {
+    background: #2b054a;
+}
+
+.error {
+    color: red;
+}
+
+.inputResult {
+    display: block;
+    margin: 20px auto;
+    border-radius: 5px;
+    border: 0;
+    padding: 8px;
+    width: 540px;
+}
+
+.inputResult:focus {
+    outline: none;
+    box-shadow: none;
+}
+
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,35 @@
+<template>
+    <input type="number" min="0" @input="changeAmount($event.target.value)" placeholder="Introduce un numero">
+</template>
+
+
+
+<script>
+export default {
+    props: {
+        changeAmount: {
+            type: Function,
+            required: true
+        }
+    }
+}
+
+</script>
+
+
+
+<style scoped>
+input {
+    display: block;
+    margin: 20px auto;
+    border-radius: 5px;
+    border: 0;
+    padding: 8px;
+    width: 540px;
+}
+input:focus {
+    outline: none;
+    box-shadow: none;
+}
+
+</style>
